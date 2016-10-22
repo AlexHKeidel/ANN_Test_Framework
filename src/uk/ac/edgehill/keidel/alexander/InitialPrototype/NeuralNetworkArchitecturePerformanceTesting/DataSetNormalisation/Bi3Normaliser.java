a16ab4b763889d7217bb0fc044bc5716e2ddc22a
Alex <alexander.keidel@go.edgehill.ac.uk>
2016-10-22 18:53:30 +0100

    Bi3 Data Conversion Added
    Next to add is the normalisation of data within the custom prototype classes datasets.

--- src/uk/ac/edgehill/keidel/alexander/InitialPrototype/NeuralNetworkArchitecturePerformanceTesting/DataSetNormalisation/Bi3Normaliser.java
+++ src/uk/ac/edgehill/keidel/alexander/InitialPrototype/NeuralNetworkArchitecturePerformanceTesting/DataSetNormalisation/Bi3Normaliser.java
@@ -30,13 +30,13 @@ public class Bi3Normaliser {
             while((currentLine = br.readLine()) != null){ //for each line in the file (stops when currentLine == null, i.e. the last line when the file ends)
                 //currentLine = currentLine.replace("\n", ""); //remove the new line from the file
                 //@TODO make sense of those data sets
-                double[] tmp = calculateFootfallOutputValues(currentLine);
-                currentLine += "," + tmp[0] + "," + tmp[1] + "\n"; //adding output values to the file
-                fullText += currentLine; //add the line to the full body of text
+//                double[] tmp = calculateFootfallOutputValues(currentLine);
+//                currentLine += "," + tmp[0] + "," + tmp[1] + "\n"; //adding output values to the file
+                fullText += normaliseToTrainingData(currentLine); //add the line to the full body of text
             }
             br.close();
 
-            fw = new FileWriter(new File(DIRECTORY + "New Supervised Demographics.csv")); //new file location for the create file
+            fw = new FileWriter(new File(DIRECTORY + "Supervised Learning Data based on Demographics.csv")); //new file location for the create file
             fw.write(fullText);
             fw.flush();
             fw.close();
@@ -46,12 +46,37 @@ public class Bi3Normaliser {
         }
     }
 
-    private double[] calculateFootfallOutputValues(String line){
+    /**
+     * @
+     * @param line
+     * @return
+     */
+    @Deprecated private double[] calculateFootfallOutputValues(String line){
         double[] outputs = new double[2];
         String[] indvValues = line.split(","); //split the line at commas
         outputs[0] = Integer.parseInt(indvValues[4]) / 8000;
         outputs[1] = Integer.parseInt(indvValues[5]) / 50;
         return outputs;
+    }
+
+    /**
+     * Takes one line of the data and converts it according to human specified logic
+     * @param oData
+     * @return
+     */
+    private String normaliseToTrainingData(String oData) { //pass old data
+        double[] supervisedLearningOutcome = new double[2];
+        String[] indvValues = oData.split(","); //split the line at commas
+        String nData = ""; //this dataset ignores the db primary key (ID) and the number of the capture device used (they are all 1), i.e. indvValues[0] and indvValues[1]
+        if(indvValues[2] == "M") nData += "1.0,"; else nData += "0.0,"; //add numeric values on male and female plus a comma delimiter
+//        nData += (double) Integer.parseInt(indvValues[3]) / 100; //age divided by 100
+//        supervisedLearningOutcome[0] = (double) Integer.parseInt(indvValues[4]) / 8000; //time in camera divided by 8000
+//        supervisedLearningOutcome[1] = (double) Integer.parseInt(indvValues[5]) / 50; //time looking at the camera divided by 50
+        nData += (double) Integer.parseInt(indvValues[3]); //age divided by 100
+        supervisedLearningOutcome[0] = (double) Integer.parseInt(indvValues[4]); //time in camera divided by 8000
+        supervisedLearningOutcome[1] = (double) Integer.parseInt(indvValues[5]); //time looking at the camera divided by 50
+        nData += supervisedLearningOutcome[0] + "," + supervisedLearningOutcome[1] + "," + supervisedLearningOutcome[0] + "," + supervisedLearningOutcome[1];
+        return nData;
     }
 
     public static void main(String[] args){
