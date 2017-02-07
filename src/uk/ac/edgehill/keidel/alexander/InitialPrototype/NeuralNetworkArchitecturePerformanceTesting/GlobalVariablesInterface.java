3e7ea8ae7791c8f3a421343ade171cfb7f89106d
Alex <alexander.keidel@go.edgehill.ac.uk>
2017-02-07 16:13:53 +0000

    added dynamic values
    such as file path.\nAlso updated standard deviation.

--- src/uk/ac/edgehill/keidel/alexander/InitialPrototype/NeuralNetworkArchitecturePerformanceTesting/GlobalVariablesInterface.java
+++ src/uk/ac/edgehill/keidel/alexander/InitialPrototype/NeuralNetworkArchitecturePerformanceTesting/GlobalVariablesInterface.java
@@ -6,7 +6,7 @@ import java.net.URL;
  * Created by Alexander Keidel, 22397868 on 21/09/2016.
  */
 public interface GlobalVariablesInterface { //all values contained here are "public static final"
-    String DEFAULT_FILE_PATH = "D:/OneDrive/Edge Hill/Final Year Project/NeurophPrototypes/";
+    String DEFAULT_FILE_PATH = System.getProperty("user.dir").replace("\\", "/") + "\\"; //"D:/OneDrive/Edge Hill/Final Year Project/NeurophPrototypes/";
             //"C:/Users/Alex/OneDrive/Edge Hill/Final Year Project/NeurophPrototypes/";
             // make sure this coincides with the project path on the machine!
     String DEFAULT_TEST_SET_NAME = "Default Perceptron Test Set.csv";
--- src/uk/ac/edgehill/keidel/alexander/InitialPrototype/NeuralNetworkArchitecturePerformanceTesting/InitialPrototype.java
+++ src/uk/ac/edgehill/keidel/alexander/InitialPrototype/NeuralNetworkArchitecturePerformanceTesting/InitialPrototype.java
@@ -6,10 +6,13 @@ package uk.ac.edgehill.keidel.alexander.InitialPrototype.NeuralNetworkArchitectu
  * The idea is to have a testing framework to decide on the best suited artificial neural network architecture for any
  * given data set.
  */
-public class InitialPrototype implements GlobalVariablesInterface{
-    public static void main(String[] args){
+public class InitialPrototype implements GlobalVariablesInterface {
+    public static void main(String[] args) {
+        String workingDirectory = System.getProperty("user.dir");
+        workingDirectory = workingDirectory.replace("\\", "/"); //replace all backward slashes with forward slashes to be used for file paths. See http://stackoverflow.com/questions/1701839/string-replaceall-single-backslashes-with-double-backslashes and http://stackoverflow.com/questions/4871051/getting-the-current-working-directory-in-java
+        System.out.println("Working Directory = " + workingDirectory);
         CustomPerceptron cp = new CustomPerceptron();
-        cp.createAndTestNeuralNetworkStructures(5,1, "trainingset name", "testset name", EXPECTED_STANDARD_DEVIATION_LIMIT);
+        cp.createAndTestNeuralNetworkStructures(5, 1, "trainingset name", "testset name", EXPECTED_STANDARD_DEVIATION_LIMIT);
         /**
          * See http://stackoverflow.com/questions/2914375/getting-file-path-in-java for saving in current file location
          */
@@ -17,7 +20,7 @@ public class InitialPrototype implements GlobalVariablesInterface{
         try {
             cp.saveCurrentNetworkSettingsToFile();
             cp.saveCurrentPerceptronToFile();
-        } catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
